af222c125e538be726432abbe1eeac26c9bc44ba
JC Chia <jingcheng19986630@outlook.com>
2021-10-20 18:25:08 +0800

    Changed stopping while capturing to run slower from start to end point

--- robot_arm_controller/src/conveyor_belt_motor_test_run/conveyor_belt_motor_test_run.ino
+++ robot_arm_controller/src/conveyor_belt_motor_test_run/conveyor_belt_motor_test_run.ino
@@ -68,39 +68,40 @@ void setup() {
 
 
   run_motor();
-  convenyor_status = 1;
+  pick_flag_msg.data = true;
+  pick_flag_pub.publish(&pick_flag_msg);
 }
 
 void loop() {
   //wait until you are actually connected
+
   while (!nh.connected())
   {
     nh.logwarn("Arduino ROS Node not connected");
     nh.spinOnce();
   }
   nh.spinOnce();
+  run_motor();
 }
 
 void run_motor(){
-  nh.loginfo("tick");
+  nh.logwarn("tick");
   myMotor->run(FORWARD);
-  myMotor->setSpeed(52);
-  delay(1000);
+  myMotor->setSpeed(45);
+  delay(1900);
   myMotor->setSpeed(0); 
 }
 
 void cameraDetectCB(const std_msgs::Bool& msg){
   if (msg.data == true && convenyor_status == 1){
     convenyor_status = 0;
-    run_motor();
-    pick_flag_msg.data = true;
-    pick_flag_pub.publish(&pick_flag_msg);
   }
 }
 
 void robotInOperationCB(const std_msgs::Bool& msg){
-  if (msg.data == false && convenyor_status == 0){
-    convenyor_status = 1;
+  if (msg.data == false){
     run_motor();
+    pick_flag_msg.data = true;
+    pick_flag_pub.publish(&pick_flag_msg);
   }
 }
